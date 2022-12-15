33161534bb93f60c9412dab6a8e00c68fed77d22
Néstor Soriano <konamiman@konamiman.com>
2022-12-15 17:39:21 +0100

    WIP: Printing output bytes when generating listing file

--- Assembler/ListingFileGenerator.cs
+++ Assembler/ListingFileGenerator.cs
@@ -4,6 +4,8 @@ namespace Konamiman.Nestor80.Assembler
 {
     public static class ListingFileGenerator
     {
+        const int bytesPerRow = 4;
+
         private static Dictionary<AddressType, string> addressSuffixes = new() {
             { AddressType.CSEG, "'" },
             { AddressType.DSEG, "\"" },
@@ -15,6 +17,10 @@ namespace Konamiman.Nestor80.Assembler
         static AddressType currentLocationArea;
         static Dictionary<AddressType, ushort> locationCounters;
         static bool isAbsoluteBuild;
+
+        static IProducesOutput currentOutputProducerLine;
+        static int outputBytesPrintedTotal;
+        static int nextRelocatableIndex;
 
         public static int GenerateListingFile(AssemblyResult assemblyResult, StreamWriter writer)
         {
@@ -30,6 +36,10 @@ namespace Konamiman.Nestor80.Assembler
 
             foreach(var resultItem in assemblyResult.ProcessedLines) {
                 ProcessLine(resultItem);
+
+                while(currentOutputProducerLine is not null) {
+                    ProcessCurrentOutputLine();
+                }
             }
 
             writer.Flush();
@@ -41,7 +51,7 @@ namespace Konamiman.Nestor80.Assembler
             var mustPrintAddress = processedLine.Label is not null;
             ushort increaseLocationCounterBy = 0;
             AddressType? changeAreaTo = null;
-            
+            var writtenOutputCharsCount = 0;
 
             if(processedLine is ChangeAreaLine cal) {
                 if(!isAbsoluteBuild) {
@@ -50,8 +60,15 @@ namespace Konamiman.Nestor80.Assembler
                 mustPrintAddress = cal.NewLocationArea is not AddressType.COMMON;
             }
             else if(processedLine is IProducesOutput ipo) {
-                mustPrintAddress = true;
-                increaseLocationCounterBy = (ushort)ipo.OutputBytes.Length; 
+                if(ipo.OutputBytes.Length > 0) {
+                    currentOutputProducerLine = ipo;
+                    outputBytesPrintedTotal = 0;
+                    nextRelocatableIndex = 0;
+                    return;
+                }
+                else {
+                    mustPrintAddress = true;
+                }
             }
             else if(processedLine is DefineSpaceLine dsl) {
                 mustPrintAddress = true;
@@ -60,13 +77,8 @@ namespace Konamiman.Nestor80.Assembler
             else if(processedLine is ChangeOriginLine col) {
                 locationCounters[currentLocationArea] = col.NewLocationCounter;
             }
-            
-            if(mustPrintAddress) {
-                writer.Write($"  {locationCounters[currentLocationArea]:X4}{addressSuffixes[currentLocationArea]} ");
-            }
-            else {
-                writer.Write("        ");
-            }
+
+            PrintLineAddress(mustPrintAddress);
 
             if(changeAreaTo.HasValue) {
                 currentLocationArea = changeAreaTo.Value;
@@ -76,6 +88,70 @@ namespace Konamiman.Nestor80.Assembler
             }
 
             writer.WriteLine(processedLine.Line);
+        }
+
+        private static void ProcessCurrentOutputLine()
+        {
+            var printedBytes = 0;
+            var outputIndex = 0;
+            var printedChars = 0;
+            var mustPrintSource = outputBytesPrintedTotal == 0;
+
+            PrintLineAddress(true);
+
+            while(printedBytes < bytesPerRow) {
+                if(nextRelocatableIndex < currentOutputProducerLine.RelocatableParts.Length && currentOutputProducerLine.RelocatableParts[nextRelocatableIndex].Index == outputIndex) {
+                    var relocatable = currentOutputProducerLine.RelocatableParts[nextRelocatableIndex];
+                    var relocatableSize = relocatable.IsByte ? 1 : 2;
+                    if(printedBytes + relocatableSize > bytesPerRow) {
+                        break;
+                    }
+
+                    if(relocatable is LinkItemsGroup lig) {
+                        writer.Write(relocatableSize == 1 ? "00*" : "0000*");
+                        printedChars += relocatableSize == 1 ? 3 : 5;
+                    }
+                    else {
+                        var relocatableAddress = (RelocatableAddress)relocatable;
+                        var value = relocatableAddress.Value;
+                        writer.Write(relocatableSize == 1 ? $"{value:X2}" : $"{value:X4}");
+                        writer.Write(addressSuffixes[relocatableAddress.Type]);
+                        printedChars += relocatableSize == 1 ? 3 : 5;
+                    }
+
+                    printedBytes += relocatableSize;
+                    outputBytesPrintedTotal += relocatableSize;
+                    outputIndex += relocatableSize;
+                }
+                else {
+                    var byteToPrint = currentOutputProducerLine.OutputBytes[outputIndex];
+                    writer.Write($"{byteToPrint:X2} ");
+                    outputIndex++;
+                    printedChars++;
+                }
+            }
+
+            locationCounters[currentLocationArea] += (ushort)printedBytes;
+
+            if(mustPrintSource) {
+                writer.WriteLine(((ProcessedSourceLine)currentOutputProducerLine).Line);
+            }
+
+            if(printedBytes >= bytesPerRow) {
+                currentOutputProducerLine = null;
+            }
+        }
+
+        private static void PrintLineAddress(bool printAddress)
+        {
+            if(printAddress) {
+                writer.Write($"  {locationCounters[currentLocationArea]:X4}{addressSuffixes[currentLocationArea]} ");
+            }
+            else {
+                writer.Write("        ");
+            }
+
+            writer.Write("   ");
         }
     }
 }
