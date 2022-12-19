6849d8990aaf57b1bd87b54ae0b0fa67e4ca1d37
Néstor Soriano <konamiman@konamiman.com>
2022-12-19 10:13:23 +0100

    Add the publish profiles, the build.sh and the zip.sh scripts

---
+++ N80/zip.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# This script needs to be run from its own directory!
+
+# Generate ZIP files for all the variants inside the <project root>/Release/zips directory.
+# You need to run build.sh first in order to actually have something to zip!
+
+output() {
+	echo "$(tput setaf "$1")$2$(tput sgr0)"
+}
+
+banner() {
+	echo
+	output 6 "----- $1 -----"
+	echo
+}
+
+set -e
+
+if [ -z "$(which zip)" ]; then
+	output 1 "*** zip is not installed!"
+	exit 1
+fi
+
+version=$(grep 'AssemblyVersion' N80.csproj | grep -Eo '[0-9.]+')
+
+cd Release
+mkdir -p zips
+rm -rf zips/*
+
+for DIR in $(find * -type d ! -path "zips" ! -path "FrameworkDependant" ! -path "SelfContained"); do
+	banner $DIR
+	zip -r -j zips/N80_${version}_$(echo $DIR | tr '/' '_').zip $DIR
+done
+
+cd -
+
+echo
+output 3 "ZIPs generation succeeded!"
