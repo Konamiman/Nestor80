a410f1c7523cc61235102aeb6fcaddcd24a4b175
Néstor Soriano <konamiman@konamiman.com>
2023-03-06 13:30:04 +0100

    Remove main_old from the linker

--- LK80/Program.cs
+++ LK80/Program.cs
@@ -1010,54 +1010,6 @@ namespace Konamiman.Nestor80.LK80
             return version;
         }
 
-
-
-        static int Main_old(string[] args)
-        {
-            var config = new LinkingConfiguration() {
-                FillingByte = 0, //0xFF,
-                OpenFile = fileName => {
-                    if(File.Exists(fileName)) {
-                        return File.OpenRead(fileName);
-                    }
-                    else {
-                        return null;
-                    }
-                },
-                GetFullNameOfRequestedLibraryFile = fileName => 
-                    Path.Combine(@"c:\users\Nesto\Nestor80", fileName) + (Path.HasExtension(fileName) ? "" : ".REL"),
-                LinkingSequenceItems = new ILinkingSequenceItem[] {
-                    /*
-                    //new SetCodeSegmentAddress() {Address = 0},
-                    //new SetCodeBeforeDataMode(),
-                    //new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\empty.rel", DisplayName = "empty.rel"},
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple.rel", DisplayName = "simple.rel"},
-                    //new SetDataBeforeCodeMode(),
-                    //new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\empty.rel", DisplayName = "empty.rel"},
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple2.rel", DisplayName = "simple2.rel"},
-                    //new SetCodeSegmentAddress() {Address = 0x103},
-                    //new SetDataSegmentAddress() {Address = 0x230},
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple3.rel", DisplayName = "simple3.rel"},
-                    //new SetCodeSegmentAddress() {Address = 0xFFF5},
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple4.rel", DisplayName = "simple4.rel"},
-                    */
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple5.rel", DisplayName = "simple5.rel"},
-                    new RelocatableFileReference() {FullName=@"c:\users\nesto\Nestor80\simple6.rel", DisplayName = "simple6.rel"}
-
-                },
-                StartAddress = 0x100,
-                //EndAddress = 0x200
-            };
-
-            var outputStream = File.Create(@"c:\users\nesto\Nestor80\simple_lk80.com");
-            var result = RelocatableFilesProcessor.Link(config, outputStream);
-            outputStream.Close();
-
-            var stream = File.OpenRead(args[0]);
-            var parsed = RelocatableFileParser.Parse(stream);
-            return 0;
-        }
-
         class CantOpenFileException : Exception
         {
             public CantOpenFileException(string filePath, string message) : base(message)
