8aafc1e66da968eb69133900b7068f42c7c4f93a
Néstor Soriano <konamiman@konamiman.com>
2026-08-17 16:16:21 +0900

    Fix spurious warning for a comment after IRPC arguments (#47)

--- Assembler/Infrastructure/SourceLineWalker.cs
+++ Assembler/Infrastructure/SourceLineWalker.cs
@@ -649,6 +649,7 @@ namespace Konamiman.Nestor80.Assembler.Infrastructure
                 args.Add(theChar.ToString());
             }
 
+            SkipBlanks();
             return (args.ToArray(), delimiterNestingLevel);
         }
 
